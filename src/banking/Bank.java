63fc7a855e9e9b38c144c85c64bce89eb71ff1a0
suresh <sklamgade47@gmail.com>
2017-02-09 08:23:59 -0600

    Format Changed

--- src/banking/Bank.java
+++ src/banking/Bank.java
@@ -1,92 +1,91 @@
 package banking;
 
-import java.util.List;
-import java.util.Optional;
-
-import javax.swing.UIManager;
-
 import banking.gui.view.BankFrm;
 import datastructure.DStructure;
 import framework.Account;
 import framework.Customer;
 import framework.Entry;
-import framework.IAccount;
 import framework.IEntry;
+
+import javax.swing.*;
+import java.util.List;
+import java.util.Optional;
 
 public class Bank {
 
-	List<Account> accounts;
+    List<Account> accounts;
 
-	public Bank() {
-		DStructure dis = DStructure.getInstance();
-		this.accounts = dis.getList();
-	}
+    public Bank() {
 
-	public void addAccount(Account account) {
-		accounts.add(account);
-	}
+        DStructure dis = DStructure.getInstance();
+        this.accounts = dis.getList();
+    }
 
-	public void addInterestToAllAccounts() {
-		accounts.forEach(a -> a.addInterest());
-	}
+    static public void main(String args[]) {
+        try {
+            // Add the following code if you want the Look and Feel
+            // to be set to the Look and Feel of the native system.
 
-	public List<Account> getAccounts() {
-		return accounts;
-	}
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e) {
+            }
 
-	public void createAccount(Customer customer, Account account) {
+            // Create a new instance of our application's frame, and make it
+            // visible.
+            (new BankFrm()).setVisible(true);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            // Ensure the application exits with an error condition.
+            System.exit(1);
+        }
+    }
 
-	}
+    public void addAccount(Account account) {
+        accounts.add(account);
+    }
 
-	public void deposit(double amount, Account account) {
-		System.out.println("Current Balance: " + account.getCurrentBalance());
-		IEntry entry = new Entry(amount, account.getCustomer().getName());
-		account.addEntry(entry);
-		account.deposit(amount);
+    public void addInterestToAllAccounts() {
+        accounts.forEach(a -> a.addInterest());
+    }
 
-		Optional<Account> aa = DStructure.getInstance().getList().stream()
-				.filter(acc -> acc.getAccountNumber().equals(account.getAccountNumber())).findFirst();
+    public List<Account> getAccounts() {
+        return accounts;
+    }
 
-		if (aa.isPresent()) {
-			System.out.println("After Balance: " + aa.get().getCurrentBalance());
-		}
+    public void createAccount(Customer customer, Account account) {
 
-	}
-	
-	public void withdraw(double amount, Account account) {
-		System.out.println("Current Balance: "+account.getCurrentBalance());
-		IEntry entry = new Entry(amount, account.getCustomer().getName());
-		account.addEntry(entry);
-		account.withdraw(amount);
+    }
 
-		
-		Optional<Account> aa = DStructure.getInstance().getList().stream()
-				.filter(acc -> acc.getAccountNumber().equals(account.getAccountNumber())).findFirst();
+    public void deposit(double amount, Account account) {
+        System.out.println("Current Balance: " + account.getCurrentBalance());
+        IEntry entry = new Entry(amount, account.getCustomer().getName());
+        account.addEntry(entry);
+        account.deposit(amount);
 
-		if (aa.isPresent()) {
-		System.out.println("After Balance: "+aa.get().getCurrentBalance());
-		}
+        Optional<Account> aa = DStructure.getInstance().getList().stream()
+                .filter(acc -> acc.getAccountNumber().equals(account.getAccountNumber())).findFirst();
 
-	}
-	
-	static public void main(String args[]) {
-		try {
-			// Add the following code if you want the Look and Feel
-			// to be set to the Look and Feel of the native system.
+        if (aa.isPresent()) {
+            System.out.println("After Balance: " + aa.get().getCurrentBalance());
+        }
 
-			try {
-				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			} catch (Exception e) {
-			}
+    }
 
-			// Create a new instance of our application's frame, and make it
-			// visible.
-			(new BankFrm()).setVisible(true);
-		} catch (Throwable t) {
-			t.printStackTrace();
-			// Ensure the application exits with an error condition.
-			System.exit(1);
-		}
-	}
+    public void withdraw(double amount, Account account) {
+        System.out.println("Current Balance: " + account.getCurrentBalance());
+        IEntry entry = new Entry(amount, account.getCustomer().getName());
+        account.addEntry(entry);
+        account.withdraw(amount);
+
+
+        Optional<Account> aa = DStructure.getInstance().getList().stream()
+                .filter(acc -> acc.getAccountNumber().equals(account.getAccountNumber())).findFirst();
+
+        if (aa.isPresent()) {
+            System.out.println("After Balance: " + aa.get().getCurrentBalance());
+        }
+
+    }
 
 }
